dedcb359f4bc0782b8a6145c7084cda31e85b14d
tungxdnd <dinhkhamhubt@gmail.com>
2022-03-23 10:08:02 +0700

    API xem thong tin nha

---
+++ app/Http/Resources/HouseRuleResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class HouseRuleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'icon' => $this->icon,
+            'checked' => $this->checked
+        ];
+    }
+}
